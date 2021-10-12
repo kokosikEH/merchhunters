18299932e9d52ae73b956aad4d11b2121b40a016
Елизавета Борисенко <m2010326@edu.misis.ru>
2021-10-13 02:30:11 +0300

    dev: добавление изображений добавление аутентификации без бэка

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using merchhunters.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace merchhunters.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        public static bool IsLogIn=false; 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -27,7 +30,27 @@ namespace merchhunters.Controllers
         {
             return View();
         }
+        public async Task<IActionResult> AccountAsync(int ID=-1)
+        {
+            ViewData["IsLogIn"] = IsLogIn;
+            IsLogIn = !IsLogIn;
+            //вызов метода из бека чтобы вкинуть айди
+            if (ID == -1)
+                return View();
+            var claims = new List<Claim>
+            {
+                new Claim(type: ClaimsIdentity.DefaultNameClaimType, ID.ToString())
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+            return RedirectToAction("Index", "Home");
 
+        }
+        public async Task<IActionResult> LogOutAsync()
+        {
+            await HttpContext.SignOutAsync();
+            return View("Index");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,12 @@ namespace merchhunters
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => //CookieAuthenticationOptions
+                {
+                    options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Home/Account");
+                });
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +50,7 @@ namespace merchhunters
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

5bc9928a434961f248a339f5b1df14d6b2c537af
NeAlyssa <alyssa_semenova@mail.ru>
2021-10-13 21:17:15 +0300

    dev: Добавила структуру проекта.

---
+++ Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace merchhunters.Common.Entities
+{
+    public class Book
+    {
+        public int RecId { get; set; } //модификатор доступа и стринг
+        public string Aut { get; set; }
+        public string Title { get; set; }
+        public string Place { get; set; }
+        public string Publ { get; set; }
+        public int Yea { get; set; }
+        public string Lan { get; set; }
+        public RubricType Rubric { get; set; }
+        public string Person { get; set; }
+        public string Serial { get; set; }
+        public string Material { get; set; }
+        public string BibLevel { get; set; }
+        public Nullable<int> Ager { get; set; }
+    }
+}
---
+++ Entities/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merchhunters.Common.Entities
+{
+    public class Reader
+    {
+        public int Abis_id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+    }
+}

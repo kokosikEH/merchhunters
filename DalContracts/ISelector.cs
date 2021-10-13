0d9765b4c22c55644906e4f9b4015f6a1c5bf74f
NeAlyssa <alyssa_semenova@mail.ru>
2021-10-13 21:47:50 +0300

    dev: Пофиксили баги.

--- DalContracts/ISelector.cs
+++ DalContracts/ISelector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using merchhunters.Common.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace merchhunters.DAL.DalContracts
 {
     interface ISelector
     {
-        Array<Recomendation> Select(Request request);
+        Recomendation[] Select(Request request);
     }
 }
